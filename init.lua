2f751538ed6e82417ee9ab73af043aed3717f5bd
arjunhm <hm.arjun97@gmail.com>
2024-10-17 16:19:17 -0500

    added config

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("no-neck-pain").setup({
+  width = 120,
+  fallbackOnBufferDelete = true,
+  autocmds = {
+    enableOnVimEnter = true,
+    reloadOnColorSchemeChange = true,
+  },
+  buffers = {
+    scratchPad = {
+      enabled = true,
+    },
+  },
+})
---
+++ lua/plugins/main.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
