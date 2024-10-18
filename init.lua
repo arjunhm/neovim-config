639b02430e7b7356af96883eececa02117f92b39
arjunhm <hm.arjun97@gmail.com>
2024-10-18 12:34:27 -0500

    add keymaps

--- init.lua
+++ init.lua
@@ -1,6 +1,57 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+require("catppuccin").setup({
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = {
+    -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = false, -- disables setting the background color.
+  show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  },
+  no_italic = false, -- Force no italic
+  no_bold = false, -- Force no bold
+  no_underline = false, -- Force no underline
+  styles = {
+    -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  default_integrations = true,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    notify = false,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
 require("no-neck-pain").setup({
   width = 120,
   fallbackOnBufferDelete = true,
@@ -14,3 +65,15 @@ require("no-neck-pain").setup({
     },
   },
 })
+
+--vim.cmd.colorscheme "catppuccin"
+vim.keymap.set("n", "<leader>nnp", ":NoNeckPain<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>nto", ":NvimTreeOpen<CR>", { silent = true })
+vim.opt.number = true
+vim.opt.foldlevel = 0
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.statuscolumn = "%s %l %r"
